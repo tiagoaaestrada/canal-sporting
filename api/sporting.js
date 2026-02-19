4627b2fb6108fcca0ea8cd48c1f263e0159cb71b
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-02-19 13:20:32 +0000

    Update sporting.js

--- api/sporting.js
+++ api/sporting.js
@@ -1,6 +1,6 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   res.status(200).json({
     title: "Sporting vence por 3-0 no último jogo!",
     link: "https://www.record.pt"
   });
-}
+};

0ef0572a7701f9efbf35c9e8a22389a5f8598bbf
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-02-19 13:10:28 +0000

    Update sporting.js

--- api/sporting.js
+++ api/sporting.js
@@ -1,25 +1,18 @@
 export default async function handler(req, res) {
   try {
-    const rssUrl =
-      "https://news.google.com/rss/search?q=Sporting+CP&hl=pt-PT&gl=PT&ceid=PT:pt";
+    const response = await fetch("https://www.record.pt/rss/futebol/sporting");
+    const xml = await response.text();
 
-    const response = await fetch(rssUrl);
-    const text = await response.text();
+    const titleMatch = xml.match(/<title><!\[CDATA\[(.*?)\]\]><\/title>/);
 
-    const titleMatch = text.match(/<title>(.*?)<\/title>/g);
-
-    if (!titleMatch || titleMatch.length < 2) {
-      return res.status(500).json({ error: "Sem notícias" });
+    if (!titleMatch) {
+      return res.status(500).json({ error: "Notícia não encontrada" });
     }
 
-    const latestTitle = titleMatch[1]
-      .replace("<title>", "")
-      .replace("</title>", "");
+    const title = titleMatch[1];
 
-    res.status(200).json({
-      title: latestTitle,
-    });
+    res.status(200).json({ title });
   } catch (error) {
-    res.status(500).json({ error: "Erro ao carregar notícia." });
+    res.status(500).json({ error: "Erro ao buscar notícia" });
   }
 }

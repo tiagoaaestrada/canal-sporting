63886b48bf220c5ad7cf485b76bbb9f403704314
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-02-19 13:29:11 +0000

    Update sporting.js

--- api/sporting.js
+++ api/sporting.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
   res.status(200).json({
     title: "Sporting vence por 3-0 no último jogo!",
-    link: "https://www.record.pt"
+    link: "https://www.record.pt/futebol/futebol-nacional/liga-betclic/sporting?ref=HP_MenuDestaque"
   });
 };

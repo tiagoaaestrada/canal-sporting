398dc1a371cbbaa2a16cb2db11da0981f775c687
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-02-19 15:09:18 +0000

    Update bola.js

--- api/bola.js
+++ api/bola.js
@@ -1,14 +1,18 @@
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   res.status(200).json({
-    cover: "https://upload.wikimedia.org/wikipedia/commons/1/13/A_Bola_logo.png",
+    cover: "https://static.abola.pt/img/capas/abola.jpg",
     news: [
       {
         title: "Eleições Sporting: Varandas apresenta programa",
-        link: "https://www.abola.pt"
+        link: "https://www.abola.pt/clubes/sporting"
       },
       {
         title: "Sporting: três pérolas da formação",
-        link: "https://www.abola.pt"
+        link: "https://www.abola.pt/clubes/sporting"
+      },
+      {
+        title: "Varandas confirma continuidade do projeto",
+        link: "https://www.abola.pt/clubes/sporting"
       }
     ]
   });

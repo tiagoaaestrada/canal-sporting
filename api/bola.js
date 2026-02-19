37429c0949bccb13014c36313bfb51c5068a3484
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-02-19 15:01:27 +0000

    Update bola.js

--- api/bola.js
+++ api/bola.js
@@ -1,9 +1,13 @@
 module.exports = async (req, res) => {
   res.status(200).json({
-    cover: "https://www.abola.pt/img/capas/abola.jpg",
+    cover: "https://upload.wikimedia.org/wikipedia/commons/1/13/A_Bola_logo.png",
     news: [
       {
-        title: "Eleições Sporting: Varandas apresenta programa de ação",
+        title: "Eleições Sporting: Varandas apresenta programa",
+        link: "https://www.abola.pt"
+      },
+      {
+        title: "Sporting: três pérolas da formação",
         link: "https://www.abola.pt"
       }
     ]

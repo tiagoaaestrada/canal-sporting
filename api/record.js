a01a0895ed8b7e76bdc2c460a60e1af6aacf13f4
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-02-19 14:58:08 +0000

    Update record.js

--- api/record.js
+++ api/record.js
@@ -1,20 +1,11 @@
 module.exports = async (req, res) => {
-  const rss = "https://news.google.com/rss/search?q=Sporting+CP+site:record.pt&hl=pt-PT&gl=PT&ceid=PT:pt";
-
-  const response = await fetch(rss);
-  const text = await response.text();
-
-  const items = text.match(/<item>(.*?)<\/item>/gs);
-
-  if (!items) {
-    return res.status(500).json({ error: "Sem notícias Record." });
-  }
-
-  const noticias = items.slice(0, 5).map(item => {
-    const title = item.match(/<title>(.*?)<\/title>/)?.[1];
-    const link = item.match(/<link>(.*?)<\/link>/)?.[1];
-    return { title, link };
+  res.status(200).json({
+    cover: "https://www.record.pt/resources/capas/record.jpg",
+    news: [
+      {
+        title: "Operação Renovações: Sporting tem (quase) todo o plantel seguro mas ainda há casos...",
+        link: "https://www.record.pt/futebol/futebol-nacional/liga-betclic/sporting"
+      }
+    ]
   });
-
-  res.status(200).json(noticias);
 };

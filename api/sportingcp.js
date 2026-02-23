1698c4ad9d53f4fc14731ba4bbbe825a35919eee
tiagoaaestrada <tiago.estrada@hotmail.com>
2026-02-23 11:49:25 +0000

    Update and rename sporting.js to sportingcp.js

---
+++ api/sportingcp.js
@@ -0,0 +1,30 @@
+module.exports = async (req, res) => {
+  try {
+    const rssUrl = "https://nitter.net/SportingCP/rss";
+
+    const response = await fetch(rssUrl);
+    const xml = await response.text();
+
+    const items = [...xml.matchAll(/<item>([\s\S]*?)<\/item>/g)];
+
+    const news = items.slice(0, 5).map(item => {
+      const title = item[1].match(/<title>(.*?)<\/title>/)?.[1] || "";
+      const link = item[1].match(/<link>(.*?)<\/link>/)?.[1] || "#";
+
+      return { title, link };
+    });
+
+    res.status(200).json({
+      cover: "/x.png",
+      coverLink: "https://x.com/SportingCP",
+      news
+    });
+
+  } catch {
+    res.status(200).json({
+      cover: "/x.png",
+      coverLink: "https://x.com/SportingCP",
+      news: []
+    });
+  }
+};
